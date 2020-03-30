6df9560d631b38613e6918cc3c61779f636af266
Vlada-Hudyma <vlada.hudyma6@gmail.com>
2020-03-30 16:41:41 +0300

    new commit

--- assets/scripts/classes/burgerMenu.js
+++ assets/scripts/classes/burgerMenu.js
@@ -13,20 +13,13 @@ let home = {
     });
   },
   HiddenText() {
+    $('.allContent').show();
     $('.buttonBox').on('click', function () {
       $(this).parent().parent().parent().find('.allContent').slideToggle();
     })
   },
 ListParallax() {
-  $('.parallax').parallax({imageSrc: './assets/image/буча.png', speed: 0.7});
-  $('.parallax1').parallax({imageSrc: './assets/image/ирпень.png', speed: 0.7});
-  $('.parallax2').parallax({imageSrc: './assets/image/obolonSky.png', speed: 0.7});
-  $('.parallax3').parallax({imageSrc: './assets/image/файнаТаун.png', speed: 0.7});
-  $('.parallax4').parallax({imageSrc: './assets/image/галактика.png', speed: 0.7});
-  $('.parallax5').parallax({imageSrc: './assets/image/паркЛенд.png', speed: 0.7});
-  $('.parallax6').parallax({imageSrc: './assets/image/бульвар.png', speed: 0.7});
-  $('.parallax7').parallax({imageSrc: './assets/image/осыково.png', speed: 0.7});
-  $('.parallax8').parallax({imageSrc: './assets/image/варшавский.png', speed: 0.7});
+  $('.image').parallax({speed: 0.7});
   }
 };
 
